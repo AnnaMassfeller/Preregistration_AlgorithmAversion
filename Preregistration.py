1527bbfbc1986c8aed6cf3f287f58070707ed354
AnnaMassfeller <anna.massfeller@ilr.uni-bonn.de>
2024-10-23 09:40:29 +0200

    naming figures as in pre-reg + rqrmts.txt

--- Preregistration.py
+++ Preregistration.py
@@ -269,231 +269,6 @@ def model(DSTExperience, TechEngagement, RiskPref, Age, Farmsize, AdvisorExperie
     Delta_WTP_Q2_relTo90 = numpyro.sample("Delta_WTP_Q2_relTo90", dist.Normal(beta_AA*LatentAIAnxiety+beta_deltaTrue*True_DeltaPerformance_Q2,0.2), obs=Delta_WTP_Q2_relTo90)
        
 
-
-########################################
-##Pretest Model##
-########################################
-
-# %%due to a little error in the pretest (80 instead of 90 for the true delta perfomance), 
-#we define another model for the pre-test data
-def model_Pretest(DSTExperience, TechEngagement, RiskPref, Age, Farmsize, AdvisorExperience, #exogenous cahracteristics
-          True_DeltaPerformance_Q0, True_DeltaPerformance_Q1, True_DeltaPerformance_Q2,#treatment
-          AIA_1=None, AIA_2=None, AIA_3=None, AIA_4=None, AIA_5=None, AIA_6=None, AIA_7=None, AIA_8=None, AIA_9=None, AIA_10=None, AIA_11=None, AIA_12=None, AIA_13=None, AIA_14=None, AIA_15=None, AIA_16=None, #outcome of interest (AI Anxiety)
-          SI_1=None, SI_2=None, 
-          PE_1=None, PE_2=None, PE_3=None, PE_4=None,
-          EE_1=None, EE_2=None, EE_3=None,
-          BI_1=None, BI_2=None, BI_3=None, #outcome of interest (Behavioral Intention)
-          Delta_WTP_Q0_relTo80=None, #outcome of interest (Delta WTP_Q0)
-          Delta_WTP_Q1_relTo80=None, #outcome of interest (Delta WTP_Q1)
-          Delta_WTP_Q2_relTo80=None, #outcome of interest (Delta WTP_Q2)
-           ):
-  
-   #AIAnxiety
-    alpha_idvConst_AIA = numpyro.sample('alpha_idvConst_AIA', dist.Normal (0,1))
-    beta_DSTExperience_AIA = numpyro.sample('beta_DSTExperience_AIA', dist.Normal (0,0.5)) # we start with quite broad, uninformed priors
-    beta_TechEngagement_AIA = numpyro.sample('beta_TechEngagement_AIA', dist.Normal (0,0.5))
-    beta_RiskPref_AIA = numpyro.sample('beta_RiskPref_AIA', dist.Normal (0,0.5))
-    beta_Age_AIA = numpyro.sample('beta_Age_AIA', dist.Normal (0,0.5))
-    beta_Farmsize_AIA = numpyro.sample('beta_Farmsize_AIA', dist.Normal (0,0.5))
-    beta_AdvisorExperience_AIA = numpyro.sample('beta_AdvisorExperience_AIA', dist.Normal (0,0.5))
-    LatentAIAnxiety = numpyro.deterministic("LatentAIAnxiety",
-                                            alpha_idvConst_AIA+
-                                            beta_DSTExperience_AIA*DSTExperience + 
-                                            beta_TechEngagement_AIA*TechEngagement + 
-                                            beta_RiskPref_AIA*RiskPref + 
-                                            beta_Age_AIA*Age +
-                                            beta_Farmsize_AIA*Farmsize + 
-                                            beta_AdvisorExperience_AIA*AdvisorExperience)
-    #try individual cutpoints
-    cutpoints_AIA_1 = numpyro.sample("cutpoints_AIA_1",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]), 
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_2 = numpyro.sample("cutpoints_AIA_2",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_3 = numpyro.sample("cutpoints_AIA_3",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_4 = numpyro.sample("cutpoints_AIA_4",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_5 = numpyro.sample("cutpoints_AIA_5",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_6 = numpyro.sample("cutpoints_AIA_6",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_7 = numpyro.sample("cutpoints_AIA_7",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_8 = numpyro.sample("cutpoints_AIA_8",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_9 = numpyro.sample("cutpoints_AIA_9",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_10 = numpyro.sample("cutpoints_AIA_10",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_11 = numpyro.sample("cutpoints_AIA_11",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_12 = numpyro.sample("cutpoints_AIA_12",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_13 = numpyro.sample("cutpoints_AIA_13",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_14 = numpyro.sample("cutpoints_AIA_14",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_15 = numpyro.sample("cutpoints_AIA_15",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-    cutpoints_AIA_16 = numpyro.sample("cutpoints_AIA_16",
-                                    dist.TransformedDistribution(dist.Normal(0, 0.3).expand([6]),
-                                    OrderedTransform()))-2.5
-                                        
-    AIA_1 = numpyro.sample('AIA_1', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_1), obs = AIA_1) 
-    AIA_2 = numpyro.sample('AIA_2', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_2), obs = AIA_2)
-    AIA_3 = numpyro.sample('AIA_3', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_3), obs = AIA_3)
-    AIA_4 = numpyro.sample('AIA_4', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_4), obs = AIA_4)
-    AIA_5 = numpyro.sample('AIA_5', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_5), obs = AIA_5)
-    AIA_6 = numpyro.sample('AIA_6', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_6), obs = AIA_6)
-    AIA_7 = numpyro.sample('AIA_7', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_7), obs = AIA_7)
-    AIA_8 = numpyro.sample('AIA_8', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_8), obs = AIA_8)
-    AIA_9 = numpyro.sample('AIA_9', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_9), obs = AIA_9)
-    AIA_10 = numpyro.sample('AIA_10', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_10), obs = AIA_10)
-    AIA_11 = numpyro.sample('AIA_11', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_11), obs = AIA_11)
-    AIA_12 = numpyro.sample('AIA_12', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_12), obs = AIA_12)
-    AIA_13 = numpyro.sample('AIA_13', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_13), obs = AIA_13)
-    AIA_14 = numpyro.sample('AIA_14', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_14), obs = AIA_14)
-    AIA_15 = numpyro.sample('AIA_15', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_15), obs = AIA_15)
-    AIA_16 = numpyro.sample('AIA_16', dist.OrderedLogistic(LatentAIAnxiety, cutpoints_AIA_16), obs = AIA_16)
-
-    # ####Part 1: Definition of UTAUT statements###
-
-    # # # #Social Influence
-    # # #set up similar to AI Anxiety
-    beta_DSTExperience_SI = numpyro.sample('beta_DSTExperience_SI', dist.Normal (0,0.5)) 
-    beta_TechEngagement_SI = numpyro.sample('beta_TechEngagement_SI', dist.Normal (0,0.5))
-    beta_RiskPref_SI = numpyro.sample('beta_RiskPref_SI', dist.Normal (0,0.5))
-    beta_Age_SI = numpyro.sample('beta_Age_SI', dist.Normal (0,0.5))
-    beta_Farmsize_SI = numpyro.sample('beta_Farmsize_SI', dist.Normal (0,0.5))
-    beta_AdvisorExperience_SI = numpyro.sample('beta_AdvisorExperience_SI', dist.Normal (0,0.5))
-    mu_LatentSocialInfluence = numpyro.deterministic("mu_LatentSocialInfluence", 
-                                                     beta_DSTExperience_SI*DSTExperience + 
-                                                     beta_TechEngagement_SI*TechEngagement + 
-                                                     beta_RiskPref_SI*RiskPref + 
-                                                     beta_Age_SI*Age + 
-                                                     beta_Farmsize_SI*Farmsize + 
-                                                     beta_AdvisorExperience_SI*AdvisorExperience)
-    cutpoints_SI_1 = numpyro.sample("cutpoints_SI_1", 
-                                  dist.TransformedDistribution(dist.Normal(0, 0.5).expand([6]), 
-                                  OrderedTransform()),)-2.5
-    cutpoints_SI_2 = numpyro.sample("cutpoints_SI_2",
-                                  dist.TransformedDistribution(dist.Normal(0, 0.5).expand([6]),
-                                  OrderedTransform()),)-2.5
-    SI_1 = numpyro.sample('SI_1', dist.OrderedLogistic(mu_LatentSocialInfluence, cutpoints_SI_1), obs=SI_1)
-    SI_2 = numpyro.sample('SI_2', dist.OrderedLogistic(mu_LatentSocialInfluence, cutpoints_SI_2), obs=SI_2)
-   
- 
-    # # #PerformanceExpectancy
-    # # #set up similar to Social Influence except for additional effect of AI Anxiety on Performance Expectancy
-    beta_DSTExperience_PE = numpyro.sample('beta_DSTExperience_PE', dist.Normal (0,0.5)) 
-    beta_TechEngagement_PE = numpyro.sample('beta_TechEngagement_PE', dist.Normal (0,0.5))
-    beta_RiskPref_PE = numpyro.sample('beta_RiskPref_PE', dist.Normal (0,0.5))
-    beta_Age_PE = numpyro.sample('beta_Age_PE', dist.Normal (0,0.5))
-    beta_Farmsize_PE = numpyro.sample('beta_Farmsize_PE', dist.Normal (0,0.5))
-    beta_AdvisorExperience_PE = numpyro.sample('beta_AdvisorExperience_PE', dist.Normal (0,0.5))
-    beta_AIA_PE = numpyro.sample('beta_AIA_PE', dist.Normal (0,0.5)) #effect of AIA on Performance Expectancy
-    mu_LatentPerformanceExpectancy = numpyro.deterministic("mu_LatentPerformanceExpectancy", 
-                                                           beta_DSTExperience_PE*DSTExperience + 
-                                                           beta_TechEngagement_PE*TechEngagement + 
-                                                           beta_RiskPref_PE*RiskPref + 
-                                                           beta_Age_PE*Age +
-                                                           beta_Farmsize_PE*Farmsize + 
-                                                           beta_AdvisorExperience_PE*AdvisorExperience +
-                                                           beta_AIA_PE*LatentAIAnxiety)
-    cutpoints_PE_1 = numpyro.sample("cutpoints_PE_1", 
-                                  dist.TransformedDistribution(dist.Normal(0, 0.5).expand([6]), 
-                                  OrderedTransform()),)-2.5
-    cutpoints_PE_2 = numpyro.sample("cutpoints_PE_2",
-                                  dist.TransformedDistribution(dist.Normal(0, 0.5).expand([6]),
-                                  OrderedTransform()),)-2.5
-    cutpoints_PE_3 = numpyro.sample("cutpoints_PE_3",
-                                  dist.TransformedDistribution(dist.Normal(0, 0.5).expand([6]),
-                                  OrderedTransform()),)-2.5
-    cutpoints_PE_4 = numpyro.sample("cutpoints_PE_4",
-                                  dist.TransformedDistribution(dist.Normal(0, 0.5).expand([6]),
-                                  OrderedTransform()),)-2.5
-    PE_1 = numpyro.sample('PE_1', dist.OrderedLogistic(mu_LatentPerformanceExpectancy, cutpoints_PE_1), obs=PE_1)
-    PE_2 = numpyro.sample('PE_2', dist.OrderedLogistic(mu_LatentPerformanceExpectancy, cutpoints_PE_2), obs=PE_2)
-    PE_3 = numpyro.sample('PE_3', dist.OrderedLogistic(mu_LatentPerformanceExpectancy, cutpoints_PE_3), obs=PE_3)
-    PE_4 = numpyro.sample('PE_4', dist.OrderedLogistic(mu_LatentPerformanceExpectancy, cutpoints_PE_4), obs=PE_4)
-    
-
-    # # #EffortExpectancy
-    # # #same set up as Performance Expectancy
-    beta_DSTExperience_EE = numpyro.sample('beta_DSTExperience_EE', dist.Normal (0,0.5))
-    beta_TechEngagement_EE = numpyro.sample('beta_TechEngagement_EE', dist.Normal (0,0.5))
-    beta_RiskPref_EE = numpyro.sample('beta_RiskPref_EE', dist.Normal (0,0.5))
-    beta_Age_EE = numpyro.sample('beta_Age_EE', dist.Normal (0,0.5))
-    beta_Farmsize_EE = numpyro.sample('beta_Farmsize_EE', dist.Normal (0,0.5))
-    beta_AdvisorExperience_EE = numpyro.sample('beta_AdvisorExperience_EE', dist.Normal (0,0.5))
-    beta_AIA_EE = numpyro.sample('beta_AIA_EE', dist.Normal (0,0.5)) #effect of AIA on Effort Expectancy
-    mu_LatentEffortExpectancy = numpyro.deterministic("mu_LatentEffortExpectancy", 
-                                                      beta_DSTExperience_EE*DSTExperience +
-                                                      beta_TechEngagement_EE*TechEngagement + 
-                                                      beta_RiskPref_EE*RiskPref + 
-                                                      beta_Age_EE*Age +
-                                                      beta_Farmsize_EE*Farmsize + 
-                                                      beta_AdvisorExperience_EE*AdvisorExperience + 
-                                                      beta_AIA_EE*LatentAIAnxiety)
-    cutpoints_EE_1 = numpyro.sample("cutpoints_EE_1", 
-                                  dist.TransformedDistribution(dist.Normal(0, 0.5).expand([6]), 
-                                  OrderedTransform()),)-2.5
-    cutpoints_EE_2 = numpyro.sample("cutpoints_EE_2",
-                                  dist.TransformedDistribution(dist.Normal(0, 0.5).expand([6]),
-                                  OrderedTransform()),)-2.5
-    cutpoints_EE_3 = numpyro.sample("cutpoints_EE_3",
-                                  dist.TransformedDistribution(dist.Normal(0, 0.5).expand([6]),
-                                  OrderedTransform()),)-2.5
-    EE_1 = numpyro.sample('EE_1', dist.OrderedLogistic(mu_LatentEffortExpectancy, cutpoints_EE_1), obs=EE_1)
-    EE_2 = numpyro.sample('EE_2', dist.OrderedLogistic(mu_LatentEffortExpectancy, cutpoints_EE_2), obs=EE_2)
-    EE_3 = numpyro.sample('EE_3', dist.OrderedLogistic(mu_LatentEffortExpectancy, cutpoints_EE_3), obs=EE_3)
-
-    # # #BehavioralIntention
-    beta_SI_BI = numpyro.sample('beta_SI_BI', dist.Normal (0,0.5)) #effect of Social Influence on Behavioral Intention
-    beta_EE_BI = numpyro.sample('beta_EE_BI', dist.Normal (0,0.5)) #effect of Effort Expectancy on Behavioral Intention
-    beta_PE_BI = numpyro.sample('beta_PE_BI', dist.Normal (0,0.5)) #effect of Performance Expectancy on Behavioral Intention
-    beta_AIA_BI = numpyro.sample('beta_AIA_BI', dist.Normal (0,0.5)) #our parameter of interest: effect of AI Anxiety on Behavioral Intention
-    mu_LatentBehavioralIntention = numpyro.deterministic("mu_LatentBehavioralIntention",
-                                   (beta_SI_BI*mu_LatentSocialInfluence + 
-                                    beta_EE_BI*mu_LatentEffortExpectancy + 
-                                    beta_PE_BI*mu_LatentPerformanceExpectancy + 
-                                    beta_AIA_BI*LatentAIAnxiety))
-    cutpoints_BI_1 = numpyro.sample("cutpoints_BI_1", 
-                                  dist.TransformedDistribution(dist.Normal(0, 0.5).expand([6]), 
-                                  OrderedTransform()),)-2.5
-    cutpoints_BI_2 = numpyro.sample("cutpoints_BI_2",
-                                  dist.TransformedDistribution(dist.Normal(0, 0.5).expand([6]),
-                                  OrderedTransform()),)-2.5
-    cutpoints_BI_3 = numpyro.sample("cutpoints_BI_3",
-                                  dist.TransformedDistribution(dist.Normal(0, 0.5).expand([6]),
-                                  OrderedTransform()),)-2.5
-    BI_1 = numpyro.sample('BI_1', dist.OrderedLogistic(mu_LatentBehavioralIntention, cutpoints_BI_1), obs=BI_1)
-    BI_2 = numpyro.sample('BI_2', dist.OrderedLogistic(mu_LatentBehavioralIntention, cutpoints_BI_2), obs=BI_2)
-    BI_3 = numpyro.sample('BI_3', dist.OrderedLogistic(mu_LatentBehavioralIntention, cutpoints_BI_3), obs=BI_3)    
-
-    # ####Part 2: Definition of the Experiment###   
-    beta_AA = numpyro.sample('beta_AA', dist.Normal(0, 0.5)) # our parameter of interest = Algorithm Aversion
-    beta_deltaTrue = numpyro.sample('beta_deltaTrue', dist.Normal(0, 0.5))  # Coef determining how percentage delta performance is mapped in WTP
-    Delta_WTP_Q0_relTo80 = numpyro.sample("Delta_WTP_Q0_relTo80", dist.Normal(beta_AA*LatentAIAnxiety+beta_deltaTrue*True_DeltaPerformance_Q0,0.2), obs=Delta_WTP_Q0_relTo80) #set SD to 0.3 to ensure it only ranges from -1 to 1
-    Delta_WTP_Q1_relTo80 = numpyro.sample("Delta_WTP_Q1_relTo80", dist.Normal(beta_AA*LatentAIAnxiety+beta_deltaTrue*True_DeltaPerformance_Q1,0.2), obs=Delta_WTP_Q1_relTo80)
-    Delta_WTP_Q2_relTo80 = numpyro.sample("Delta_WTP_Q2_relTo80", dist.Normal(beta_AA*LatentAIAnxiety+beta_deltaTrue*True_DeltaPerformance_Q2,0.2), obs=Delta_WTP_Q2_relTo80)
-       
-
 #%%
 #####################################################
 #Step 2: Create synthetic data with H1 true/not true#
@@ -897,7 +672,7 @@ plt.show()
 # %%plot betas
 #therefore we need to get the samples from the mcmc grouped by chain
 
-#%%1. H1 true
+#%%1. H1 true (Figure 5&7 in Prergistration)
 samples_mcmc_H1_grouped = mcmc_synthetic_H1True.get_samples(group_by_chain=True)
 print(samples_mcmc_H1_grouped.keys())
 
@@ -919,7 +694,7 @@ xrr.add_groups(prior=dsPrior)
 xrr
 
 
-#%%for preregistration: only Algorithm Aversion betas
+#%%for preregistration: only Algorithm Aversion betas (Figre 5)
 
 ax = az.plot_posterior(
     xrr,
@@ -944,7 +719,7 @@ ax = az.plot_posterior(
     ax=ax
 )
 
-#%%trace plot for selected vars
+#%%trace plot for selected vars (Figure 7 in Preregistration)
 az.plot_trace(mcmc_synthetic_H1True,
               var_names=['beta_AIA_BI','beta_AA',
                           'beta_deltaTrue', 'alpha_idvConst_AIA'],
@@ -954,7 +729,7 @@ plt.suptitle('Trace Plot from syntehtic data where H1 is true for selected varia
 plt.show()
 
 
-# %%same for H1 not true
+# %%same for H1 not true (Figure 6&7 in Preregistration)
 samples_mcmc_NoH1_grouped = mcmc_synthetic_NoH1.get_samples(group_by_chain=True)
 print(samples_mcmc_NoH1_grouped.keys())
 
@@ -976,7 +751,7 @@ xrr.add_groups(prior=dsPrior)
 xrr
 
 
-#%%for preregistration: only Algorithm Aversion betas
+#%%for preregistration: only Algorithm Aversion betas (Figure 6)
 
 ax = az.plot_posterior(
     xrr,
@@ -1002,7 +777,7 @@ ax = az.plot_posterior(
 )
 
 
-#%%trace plot for selected vars
+#%%trace plot for selected vars (Figure 7 in Preregistration)
 az.plot_trace(mcmc_synthetic_NoH1,
               var_names=['beta_AIA_BI','beta_AA',
                           'beta_deltaTrue', 'alpha_idvConst_AIA'],
@@ -1011,3 +786,4 @@ az.plot_trace(mcmc_synthetic_NoH1,
 plt.suptitle('Trace Plot from synthetic data where H1 is not true for selected variables', y=1.09)
 plt.show()
 
+#%%
